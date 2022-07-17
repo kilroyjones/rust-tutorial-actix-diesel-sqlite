02510c7ea076db90b2894b0e6f134a19d4ba27b4
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2022-07-18 02:05:23 +0430

    Add comments and simplify the code a bit

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,11 @@
 use crate::schema::*;
 use serde::{Deserialize, Serialize};
 
+// TODO implement From<> and Into<> traits for all of the three.
+// TODO make a macro that automatically creates/guesses such structs.
+// TODO move it all to `models/link.rs`
+
+/// Represents a Link object from the database.
 #[derive(Debug, Serialize, Deserialize, Queryable)]
 pub struct Link {
     pub id: i32,
@@ -9,6 +14,7 @@ pub struct Link {
     pub date_create: String,
 }
 
+/// Represents a link that is being newly generated.
 #[derive(Debug, Insertable)]
 #[table_name = "links"]
 pub struct LinkNew<'a> {
@@ -17,6 +23,7 @@ pub struct LinkNew<'a> {
     pub date_created: &'a str,
 }
 
+/// Represents a JSON data form filled request that will turn to a `Link`.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LinkJson {
     pub link: String,
--- src/routes.rs
+++ src/routes.rs
@@ -1,3 +1,7 @@
+//! This file holds all the routes and request related functions.
+// TODO rename to handlers as it is more common in Actix Web and also handlers
+// files are expected to host services alongside routes but routes files are not
+// expected to have handlers in them.
 use crate::models::{Link, LinkJson, LinkNew};
 use crate::Pool;
 
@@ -8,28 +12,36 @@ use diesel::dsl::insert_into;
 use diesel::prelude::*;
 use diesel::RunQueryDsl;
 
+/// Returns the home page of the site.
 pub async fn home() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../templates/index.html")))
 }
 
+/// Adds a link entry to the database if not already added.
 pub async fn add_link(
     pool: web::Data<Pool>,
     item: web::Json<LinkJson>,
 ) -> Result<HttpResponse, Error> {
     Ok(
         match web::block(move || add_single_link(pool, item)).await {
-            Ok(Ok(link)) => HttpResponse::Created().json(link),
+            Ok(link) => HttpResponse::Created().json(link),
             _ => HttpResponse::from(HttpResponse::InternalServerError()),
         },
     )
 }
 
-fn add_single_link(
-    pool: web::Data<Pool>,
-    item: web::Json<LinkJson>,
-) -> Result<Link, diesel::result::Error> {
+/// Get all the links in the database.
+pub async fn get_links(pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
+    Ok(match get_all_links(pool).await {
+        Ok(links) => HttpResponse::Ok().json(links),
+        _ => HttpResponse::from(HttpResponse::InternalServerError()),
+    })
+}
+
+// TODO move to a method of the `Link`.
+fn add_single_link(pool: web::Data<Pool>, item: web::Json<LinkJson>) -> Link {
     use crate::schema::links::dsl::*;
     let db_connection = pool.get().unwrap();
 
@@ -37,7 +49,7 @@ fn add_single_link(
         .filter(link.eq(&item.link))
         .first::<Link>(&db_connection)
     {
-        Ok(result) => Ok(result),
+        Ok(result) => result,
         Err(_) => {
             let new_link = LinkNew {
                 link: &item.link,
@@ -51,18 +63,12 @@ fn add_single_link(
                 .expect("Error saving new link");
 
             let result = links.order(id.desc()).first(&db_connection).unwrap();
-            Ok(result)
+            result
         }
     }
 }
 
-pub async fn get_links(pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    Ok(match get_all_links(pool).await {
-        Ok(links) => HttpResponse::Ok().json(links),
-        _ => HttpResponse::from(HttpResponse::InternalServerError()),
-    })
-}
-
+// TODO move to a method of the `Link`.
 async fn get_all_links(pool: web::Data<Pool>) -> Result<Vec<Link>, diesel::result::Error> {
     use crate::schema::links::dsl::*;
     let db_connection = pool.get().unwrap();
